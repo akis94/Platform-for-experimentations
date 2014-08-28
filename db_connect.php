07e1bfea0703c591e65ec767f096d0b4ae020a9e
akis94 <akispro94@gmail.com>
2014-08-29 00:59:22 +0300

    db_connection
    Database connection with PHP.

---
+++ db_connect.php
@@ -0,0 +1,18 @@
+<?php //db_connect
+  //mysql_connect('localhost','root','');
+  /*
+    try{
+      if(!mysql_connect($host, $user, $pass)){
+        throw new Exception("Error connection");
+      }catch(Exception $e){
+        echo $e->getMessage();
+        echo mysql_errno() . " Error " . mysql_error() . "<br>";
+      }
+    }
+    
+    $q = mysql_query("SELECT * 
+                      FROM users
+                      WHERE id>0;");
+    mysql_fetch_array($q);
+  */
+?>
